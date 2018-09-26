364416785758a121135d27d0f539869b5426b1a2
arrenglover <arren.glover@iit.it>
2018-09-26 14:30:02 +0200

    added (commented) code for start/stop

--- spinnaker/ICUB_output_vertex/ICUB_output_vertex.py
+++ spinnaker/ICUB_output_vertex/ICUB_output_vertex.py
@@ -1,7 +1,12 @@
+from spinn_utilities.overrides import overrides
 from pacman.model.graphs.machine import MachineSpiNNakerLinkVertex
-
+from spinn_front_end_common.abstract_models \
+    import AbstractSendMeMulticastCommandsVertex
+from spinn_front_end_common.utility_models.multi_cast_command \
+    import MultiCastCommand
 
 class ICUBOutputVertex(MachineSpiNNakerLinkVertex):
+    #                   AbstractSendMeMulticastCommandsVertex):
 
     def __init__(self, label, spinnaker_link_id, board_address,
                  constraints=None):
@@ -9,3 +14,21 @@ class ICUBOutputVertex(MachineSpiNNakerLinkVertex):
             self, spinnaker_link_id=spinnaker_link_id,
             board_address=board_address, label=label, constraints=constraints)
 
+
+    # @property
+    # @overrides(AbstractSendMeMulticastCommandsVertex.start_resume_commands)
+    # def start_resume_commands(self):
+    #     return [MultiCastCommand(
+    #         key=0x80000000, payload=0, repeat=3, delay_between_repeats=100)]
+    #
+    #
+    # @property
+    # @overrides(AbstractSendMeMulticastCommandsVertex.pause_stop_commands)
+    # def pause_stop_commands(self):
+    #     return [MultiCastCommand(
+    #         key=0x40000000, payload=0, repeat=3, delay_between_repeats=100)]
+    #
+    # @property
+    # @overrides(AbstractSendMeMulticastCommandsVertex.timed_commands)
+    # def timed_commands(self):
+    #     return []
